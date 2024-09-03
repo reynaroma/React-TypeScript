1287ace47ca91f32bdfabbd260b2c8bf206d425d
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-09-03 16:53:34 -0600

    Add type on the on change event

--- src/Person.tsx
+++ src/Person.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 export interface Person {
   name: string;
@@ -7,23 +7,26 @@ export interface Person {
 }
 
 export const Person = (props: Person) => {
-
-  const [isShowInfo, setShowInfo] = useState<boolean>(false);
+  const [personBio, setPersonBio] = useState<string | null>(null);
+  // const [isShowInfo, setShowInfo] = useState<boolean>(false);
   // every time the button is clicked, the value of the state will be toggled
   // const toggeleInfo = () => {
   //   setShowInfo((prev) => !prev);
   // }
 
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPersonBio(event.target.value)
+  }
+
   return (
     <div>
-      {isShowInfo && (
-        <>
-          <p>Name: {props.name}</p>
-          <p>Age: {props.age}</p>
-          <p>This person {props.isMarried ? "is married." : "is single."}</p>
-        </>
-      )}
+      <p>Name: {props.name}</p>
+      <p>Age: {props.age}</p>
+      <p>This person {props.isMarried ? "is married." : "is single."}</p>
       {/* <button onClick={toggeleInfo}>Toggle Info</button> */}
+      {" "}
+      <p>{props.name} Bio: {!personBio ? "No Bio available" : personBio}</p>
+      <input onChange={handleChange}/>
     </div>
   )
 }
