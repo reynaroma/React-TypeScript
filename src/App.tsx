8d6ea2faa8b7ce8b3a072154af620075dc3f1ea7
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-09-03 21:30:25 -0600

    Wrap the App with UserProvider component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import './App.css';
-import { Props } from './Person';
+import { User } from './Person';
+import { UserProvider } from './UserContextProvider';
 
 function App() {
 
   return (
-    <>
+    <UserProvider>
       <User name={"Reyna"} age={27} isMarried={false} />
       <User name={"Tine"} age={26} isMarried={false} />
       <User name={"Toffee"} age={3} isMarried={false} />
-    </>
+    </UserProvider>
   )
 }
 
